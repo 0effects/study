67d96c9222f0e8918e1bf0f02e7e602f5e870c68
0effect <0effect@protonmail.com>
2019-01-15 03:38:10 +0000

    Chapter 13: Start working on issue app, added CLI parser

---
+++ elixir/ch13/issues/test/cli_test.exs
@@ -0,0 +1,19 @@
+defmodule CliTest do
+  use ExUnit.Case
+  doctest Issues
+
+  import Issues.CLI, only: [ parse_args: 1 ]
+
+  test "parsing help flags returns :help" do
+    assert parse_args(["-h", "anything"]) == :help
+    assert parse_args(["--help", "anything"]) == :help
+  end
+
+  test "parsing three options returns those three" do
+    assert parse_args(["user1", "project2", 99]) == {"user1", "project2", 99}
+  end
+
+  test "parsing two options returns them and a default count" do
+    assert parse_args(["user1", "project2"]) == {"user1", "project2", 4}
+  end
+end
