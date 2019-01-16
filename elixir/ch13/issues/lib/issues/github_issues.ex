6951893ab159273359a6cc74ae7370c95356b44a
0effect <0effect@protonmail.com>
2019-01-16 01:52:09 +0000

    Write simple github fetcher

---
+++ elixir/ch13/issues/lib/issues/github_issues.ex
@@ -0,0 +1,25 @@
+defmodule Issues.GithubIssues do
+  @moduledoc """
+  Do the work of grabbing data from github.
+  We'll hit their issues API.
+  """
+  @user_agent [{"User-agent", "Elixir dave@pragprog.com"}]
+
+  def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
+
+  def issues_url(user, project) do
+    "https://api.github.com/repos/#{user}/#{project}/issues"
+  end
+
+  def handle_response({:ok, %{status_code: 200, body: body}}) do
+    {:ok, body}
+  end
+
+  def handle_response({_, %{status_code: _, body: body}}) do
+    {:error, body}
+  end
+end
