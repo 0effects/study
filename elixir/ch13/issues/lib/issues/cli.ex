8566c4fa06216faa9ab102af697f76fbef783761
0effect <0effect@protonmail.com>
2019-01-16 01:58:10 +0000

    Add poison JSON decoder and decode responses from github

--- elixir/ch13/issues/lib/issues/cli.ex
+++ elixir/ch13/issues/lib/issues/cli.ex
@@ -21,6 +21,15 @@ defmodule Issues.CLI do
 
   def process({user, project, _count}) do
     Issues.GithubIssues.fetch(user, project)
+    |> decode_response()
+    |> IO.inspect()
+  end
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    IO.puts("Error fetching from Github: #{error["message"]}")
+    System.halt(2)
   end
 
   @doc """
