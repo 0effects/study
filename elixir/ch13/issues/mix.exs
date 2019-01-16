e1c43f1b48a8554d4eff463a9d7bd8606ce87b12
0effect <0effect@protonmail.com>
2019-01-16 02:52:16 +0000

    Make documentation with mix doc

--- elixir/ch13/issues/mix.exs
+++ elixir/ch13/issues/mix.exs
@@ -6,6 +6,8 @@ defmodule Issues.MixProject do
       app: :issues,
       escript: escript_config(),
       version: "0.1.0",
+      name: "Issues",
+      source_url: "https://github.com/0effects/study/tree/master/elixir/ch13/issues",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -24,7 +26,9 @@ defmodule Issues.MixProject do
     [
       {:httpoison, "~> 1.4"},
       {:poison, "~> 3.1"},
-      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.19"},
+      {:earmark, "~> 1.2.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
