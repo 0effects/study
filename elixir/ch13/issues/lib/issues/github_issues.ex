a5ebdf6b591ee291959074829759d09026ea480d
0effect <0effect@protonmail.com>
2019-01-16 02:46:38 +0000

    Try out the logger for debugging

--- elixir/ch13/issues/lib/issues/github_issues.ex
+++ elixir/ch13/issues/lib/issues/github_issues.ex
@@ -1,4 +1,6 @@
 defmodule Issues.GithubIssues do
+  require Logger
+
   @moduledoc """
   Do the work of grabbing data from github.
   We'll hit their issues API.
@@ -8,6 +10,8 @@ defmodule Issues.GithubIssues do
   @user_agent [{"User-agent", "Elixir dave@pragprog.com"}]
 
   def fetch(user, project) do
+    Logger.info("Fetching #{user}'s project #{project}")
+
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -18,6 +22,9 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response({_, %{status_code: status_code, body: body}}) do
+    Logger.info("Got response: status code=#{status_code}")
+    Logger.debug(fn -> inspect(body) end)
+
     {
       status_code |> check_for_error(),
       body |> Poison.Parser.parse!()
